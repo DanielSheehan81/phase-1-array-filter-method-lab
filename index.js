588cbabf2967bae810f3c2d458604150bc067818
DanielSheehan <daniel.sheehan81@yahoo.com>
2021-10-17 18:51:19 -0400

    commit tree 1c4d5a5786e969952100c23dfeb484630182c100

--- index.js
+++ index.js
@@ -1,1 +1,16 @@
 // Code your solution here
+function findMatching(driverNames, name){
+    let match = driverNames.filter(driverName => driverName.toUpperCase() === name.toUpperCase());
+    return match;
+}
+findMatching('Bobby');
+
+function fuzzyMatch(driverNames, name){
+    const result = driverNames.filter(driverName => driverName.slice(0, name.length) === name);
+    return result
+}
+
+function matchName(drivers, name){
+    let match = drivers.filter(drivers => drivers.name === name);
+    return match;
+}
